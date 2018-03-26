9aaecb8aeaaf1bb8971863e67b17b86481570bdd
yangle <mr.yang0229@foxmail.com>
2018-03-26 17:43:15 +0800

    将FFmpeg移植到Android平台

---
+++ FFmpeg4Android/app/src/main/cpp/ffmpeg_cmd.c
@@ -0,0 +1,18 @@
+#include <jni.h>
+#include "ffmpeg.h"
+
+JNIEXPORT jint
+
+JNICALL
+Java_com_yl_ffmpeg4android_MainActivity_run(
+        JNIEnv *env, jclass obj, jobjectArray commands) {
+    int argc = (*env)->GetArrayLength(env, commands);
+    char *argv[argc];
+
+    int i;
+    for (i = 0; i < argc; i++) {
+        jstring js = (jstring) (*env)->GetObjectArrayElement(env, commands, i);
+        argv[i] = (char *) (*env)->GetStringUTFChars(env, js, 0);
+    }
+    return run(argc, argv);
+}
---
+++ FFmpeg4Android/app/src/main/java/com/yl/ffmpeg4android/MainActivity.java
@@ -0,0 +1,96 @@
+package com.yl.ffmpeg4android;
+
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+import java.io.File;
+
+/**
+ * 截取gif动图
+ * <p>
+ * Created by yangle on 2018/3/26.
+ * Website：http://www.yangle.tech
+ */
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    static {
+        System.loadLibrary("ffmpeg");
+    }
+
+    private ImageView ivGif;
+    private Button btnConvert;
+    private ProgressDialog progressDialog;
+    // 设备根目录路径
+    private String path = Environment.getExternalStorageDirectory().getAbsolutePath();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ivGif = findViewById(R.id.iv_gif);
+        btnConvert = findViewById(R.id.btn_convert);
+        btnConvert.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        // 截取视频的前500帧
+        final String cmd = "ffmpeg -i " + path + "/video.mp4 -vframes 500 -y -f gif -s 480×320 " + path + "/video_500.gif";
+        // 显示loading
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setTitle("截取中...");
+        progressDialog.show();
+
+        new Thread() {
+            @Override
+            public void run() {
+                super.run();
+                // 执行指令
+                cmdRun(cmd);
+
+                // 隐藏loading
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        progressDialog.dismiss();
+                        progressDialog = null;
+
+                        // 显示gif
+                        Glide.with(MainActivity.this)
+                                .load(new File(path + "/video_500.gif"))
+                                .into(ivGif);
+                    }
+                });
+            }
+        }.start();
+    }
+
+    /**
+     * 以空格分割指令，生成String类型的数组
+     *
+     * @param cmd 指令
+     * @return 执行code
+     */
+    private int cmdRun(String cmd) {
+        String regulation = "[ \\t]+";
+        final String[] split = cmd.split(regulation);
+        return run(split);
+    }
+
+    /**
+     * ffmpeg_cmd中定义的run方法
+     *
+     * @param cmd 指令
+     * @return 执行code
+     */
+    public native int run(String[] cmd);
+}
